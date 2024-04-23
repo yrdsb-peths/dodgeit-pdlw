d7d063b84a47e55d3000675f8324bc945ac577e6
pdlw <166637497+pdlw@users.noreply.github.com>
2024-04-23 00:21:25 -0400

    Created and set up the "Banana" and Goof" classes.

---
+++ Banana.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Banana extends Actor
+{
+
+    public void act()
+    {
+        move(-3);
+        if(getX() == 0)
+        {
+            resetBanana();
+        }
+    }
+    public void resetBanana()
+    {
+        int x = Greenfoot.getRandomNumber(2);
+        if(x == 0)
+        {
+            setLocation(600,100);
+        }
+        else
+        {
+            setLocation(600,300);
+        }
+    }
+}
